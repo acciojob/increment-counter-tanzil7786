85759b92ecff8af8e901b36f385b30c0d37d8060
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-08-11 09:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+function counter() {
+  var _counter = 0;
+
+  function add(increment) {
+    _counter += increment;
+  }
+
+  function retrieve() {
+    return _counter;
+  }
+
+  return {
+    add,
+    retrieve,
+  };
+}
+
+const c = counter();
+
+const counterElement = document.getElementById('counter');
+const incrementButton = document.getElementById('incrementBtn');
+
+incrementButton.addEventListener('click', function() {
+    alert(c.retrieve());
+});
+
+// Update the counter element's content
+function updateCounterDisplay() {
+    counterElement.textContent = c.retrieve();
+}
+
+updateCounterDisplay(); // Initial update
+
+// For demonstration purposes, add values on each button click
+incrementButton.addEventListener('click', function() {
+    c.add(1);
+    updateCounterDisplay();
+});
